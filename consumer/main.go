3a6b6d241096435662685d3c451615890206a280
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2026-02-13 16:15:32 +0100

    Add mechanism to keep consuming messages and delete those after processing

--- consumer/main.go
+++ consumer/main.go
@@ -7,7 +7,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
+
+const queueURL = "http://localhost:4566/000000000000/my-queue"
 
 func main() {
 	awsRegion := "eu-west-1"
@@ -21,15 +24,36 @@ func main() {
 		o.BaseEndpoint = aws.String(awsEndpoint)
 	})
 
-	queueURL := "http://localhost:4566/000000000000/my-queue"
-	receiveMessageInput := &sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(queueURL),
-		MaxNumberOfMessages: 1,
+	for {
+		results, err := sqsClient.ReceiveMessage(
+			context.TODO(),
+			&sqs.ReceiveMessageInput{
+				QueueUrl:            aws.String(queueURL),
+				MaxNumberOfMessages: 10,
+			})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, message := range results.Messages {
+			processJob(message, sqsClient)
+		}
+	}
+}
+
+func processJob(message types.Message, sqsClient *sqs.Client) {
+
+	deleteParams := &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(queueURL),
+		ReceiptHandle: message.ReceiptHandle,
 	}
 
-	resp, err := sqsClient.ReceiveMessage(context.TODO(), receiveMessageInput)
+	log.Printf("Received message: %s", *message.Body)
+
+	_, err := sqsClient.DeleteMessage(context.TODO(), deleteParams)
+
 	if err != nil {
-		log.Fatalf("Cannot receive messages: %s", err)
+		log.Fatal(err)
 	}
-	log.Printf("Received messages: %v", resp.Messages)
 }
